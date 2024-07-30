06f234a421a9ab8321cf09084ee103cba97530e8
luongnguyenminhan <anlnmse182471@fpt.edu.vn>
2024-07-30 13:07:13 +0700

    update inference

--- inference.py
+++ inference.py
@@ -7,43 +7,6 @@ import argparse
 from Human_dataset import HumanDataset
 from unet import UNet
 
-# def pred_show_image_grid(data_path, model_pth, device):
-#     model = UNet(in_channels=3, num_classes=1).to(device)
-#     model.load_state_dict(torch.load(model_pth, map_location=torch.device(device)))
-#     image_dataset = HumanDataset(data_path, test=True)
-#     images = []
-#     orig_masks = []
-#     pred_masks = []
-
-#     for img, orig_mask in image_dataset:
-#         img = img.float().to(device)
-#         img = img.unsqueeze(0)
-
-#         pred_mask = model(img)
-
-#         img = img.squeeze(0).cpu().detach()
-#         img = img.permute(1, 2, 0)
-
-#         pred_mask = pred_mask.squeeze(0).cpu().detach()
-#         pred_mask = pred_mask.permute(1, 2, 0)
-#         pred_mask[pred_mask < 0]=0
-#         pred_mask[pred_mask > 0]=1
-
-#         orig_mask = orig_mask.cpu().detach()
-#         orig_mask = orig_mask.permute(1, 2, 0)
-
-#         images.append(img)
-#         orig_masks.append(orig_mask)
-#         pred_masks.append(pred_mask)
-
-#     images.extend(orig_masks)
-#     images.extend(pred_masks)
-#     fig = plt.figure()
-#     for i in range(1, 3*len(image_dataset)+1):
-#        fig.add_subplot(3, len(image_dataset), i)
-#        plt.imshow(images[i-1], cmap="gray")
-#     plt.show()
-
 
 def single_image_inference(image_pth, model_pth, save_pth, device):
     model = UNet(in_channels=3, num_classes=1).to(device)
@@ -66,12 +29,10 @@ def single_image_inference(image_pth, model_pth, save_pth, device):
     pred_mask[pred_mask < 0]=0
     pred_mask[pred_mask > 0]=1
 
-    # save mask using PIL
     fig = plt.figure(figsize=(10, 10))
     plt.imshow(pred_mask, cmap="gray")
     plt.axis("off")
     plt.savefig(save_pth)
-    # pred_mask.save(save_pth + "mask.jpg")
     return pred_mask
     
 if __name__ == "__main__":
@@ -88,5 +49,4 @@ if __name__ == "__main__":
     SAVE_PATH = args.save_path
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    # pred_show_image_grid(DATA_PATH, MODEL_PATH, device)
     single_image_inference(SINGLE_IMG_PATH, MODEL_PATH, SAVE_PATH, device)
